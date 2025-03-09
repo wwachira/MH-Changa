3ee6fcdcd8498915506b4ca44acb97f7b2b11307
lizwachira <lizwanguwachira@gmail.com>
2025-03-10 02:09:37 +0300

    add company logos

--- README.md
+++ README.md
@@ -15,3 +15,21 @@ npx tailwindcss init --full
 touch postcss.config.js
 npx tailwindcss init -p
 npx tailwindcss init -p
+
+to update npm if npx doesn't run: npm install -g npm
+
+# if installing npx on ur terminal try your this commands:
+on your terminal run this 3 commands, final result should show Checksums matched!: 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bash_profile
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.bash_profile
+echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"' >> ~/.bash_profile
+source ~/.bash_profile
+
+nvm install --lts
+
+# to download free logos
+Go to: https://logowik.com/ 
--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 const Logos = () => {
-  const logos = ["logo1.png", "logo2.png", "logo3.png"]; // Add your logo paths here
+  const logos = [
+    { name: 'WHO', src: 'https://www.who.int/images/default-source/fallback/logo.who.png' },
+    { name: 'UNICEF', src: 'https://www.unicef.org/sites/default/files/styles/logo/public/UNICEF_Logo_Blue.png' },
+    { name: 'Mind', src: 'https://www.mind.org.uk/media/10157/mind-logo.png' },
+    { name: 'BetterHelp', src: 'https://www.betterhelp.com/images/betterhelp_logo.svg' },
+    { name: 'Amref Health Africa', src: 'https://amref.org/wp-content/uploads/2020/04/Amref-Logo.png' },
+    { name: 'BasicNeeds Kenya', src: 'https://basicneeds.org/wp-content/uploads/2021/03/BasicNeeds-Kenya-Logo.png' },
+  ];
 
   return (
     <div className="p-10 bg-gray-200">
@@ -11,8 +18,8 @@ const Logos = () => {
         {logos.map((logo, index) => (
           <motion.img
             key={index}
-            src={logo}
-            alt="Logo"
+            src={logo.src}
+            alt={logo.name}
             className="h-16"
             animate={{ rotate: 360 }}
             transition={{ duration: 10, repeat: Infinity, ease: "linear" }}
